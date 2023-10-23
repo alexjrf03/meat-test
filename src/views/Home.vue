99312bbf31281844597673a8d6a8bf18d66a63ac
alexjrf03 <alexjrf27@gmail.com>
2023-10-23 17:20:01 -0400

    directory api created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    
+    <Hero />
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
+  components:{
+    Hero: () => import(/* webpackPrefetch: true */ '@/components/hero/Hero.vue'),
+  },
 
   data() {
     return {
