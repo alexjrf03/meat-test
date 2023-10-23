b3fdf26f58e3872c2db635d0ee07fc91318e4f39
alexjrf03 <alexjrf27@gmail.com>
2023-10-23 19:10:00 -0400

    hero completed

--- src/components/ui/Footer.vue
+++ src/components/ui/Footer.vue
@@ -1,6 +1,50 @@
 <template>
   <div>
-    
+    <h2 class="mt-6">Contáctanos</h2>
+    <v-container>
+
+        <v-row justify="center">
+          <v-col cols="12" md="4">
+            <v-text-field
+              class="inputs"
+                outlined
+            ></v-text-field>
+          </v-col>
+  
+          <v-col cols="12" md="4">
+            <v-text-field
+              outlined
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+      <v-row justify="center">
+        <v-col cols="12" md="4">
+          <v-text-field
+            class="inputs"
+            outlined
+            color="primary"
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            class="inputs"
+            outlined
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <center>
+        <v-btn
+          class="send-button"
+          rounded
+          elevation="0"
+          color="primary"
+        >
+          Enviar
+        </v-btn>
+      </center>
+    </v-container>
   </div>
 </template>
 
@@ -25,5 +69,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+  .send-button{
+    width: 170px;
+    height: 55px !important;
+  }
 
 </style>
